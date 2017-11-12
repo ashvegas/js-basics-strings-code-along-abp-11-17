8237bfd42d758c3979dd884986a722d8cb8be8d1
AC <msashvegas@gmail.com>
2017-11-12 07:46:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const currentUser = 'Sneakies';
 // Write your code in this file!
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.touppercase();
